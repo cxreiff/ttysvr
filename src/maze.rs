d255b3bd07c44ec3f8a5996ac1ad19b114efe6dc
cxreiff <cooper@cxreiff.com>
2024-08-17 16:08:34 -0700

    temporary allow dirty in cargo dist

--- src/maze.rs
+++ src/maze.rs
@@ -1,8 +1,10 @@
+use avian2d::math::PI;
 use bevy::prelude::*;
 use bevy_ratatui_render::RatatuiRenderContext;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(Startup, maze_setup_system);
+    app.add_systems(Startup, maze_setup_system)
+        .add_systems(Update, rotate_system);
 }
 
 fn maze_setup_system(
@@ -17,7 +19,8 @@ fn maze_setup_system(
                 target: ratatui_render.target("main").unwrap_or_default(),
                 ..default()
             },
-            transform: Transform::from_xyz(0., 5., 0.).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_translation(Vec3::ZERO)
+                .looking_at(Vec3::new(0., 0., 1.), Vec3::Y),
             ..default()
         })
         .with_children(|commands| {
@@ -31,8 +34,31 @@ fn maze_setup_system(
         });
 
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::from_size(Vec3::splat(1.))),
+        transform: Transform::from_xyz(0., 0., 5.),
+        mesh: meshes.add(Cuboid::from_size(Vec3::new(5., 2., 0.1))),
         material: materials.add(StandardMaterial::from_color(Color::hsl(0., 0.5, 0.5))),
         ..default()
     });
+    commands.spawn(PbrBundle {
+        transform: Transform::from_xyz(5., 0., 0.).with_rotation(Quat::from_rotation_y(PI / 2.)),
+        mesh: meshes.add(Cuboid::from_size(Vec3::new(5., 2., 0.1))),
+        material: materials.add(StandardMaterial::from_color(Color::hsl(0., 0.5, 0.5))),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        transform: Transform::from_xyz(-5., 0., 0.).with_rotation(Quat::from_rotation_y(-PI / 2.)),
+        mesh: meshes.add(Cuboid::from_size(Vec3::new(5., 2., 0.1))),
+        material: materials.add(StandardMaterial::from_color(Color::hsl(0., 0.5, 0.5))),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        transform: Transform::from_xyz(0., 0., -5.).with_rotation(Quat::from_rotation_y(PI)),
+        mesh: meshes.add(Cuboid::from_size(Vec3::new(5., 2., 0.1))),
+        material: materials.add(StandardMaterial::from_color(Color::hsl(0., 0.5, 0.5))),
+        ..default()
+    });
+}
+
+fn rotate_system(time: Res<Time>, mut camera: Query<&mut Transform, With<Camera>>) {
+    camera.single_mut().rotate_y(time.delta_seconds());
 }
